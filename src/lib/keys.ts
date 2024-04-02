2d7aba13a8940e4f6fc784b4dfae08cf1976ce96
Adrian <adrhc@outlook.com>
2024-04-02 17:53:25 +0300

    onEnterKey

--- src/lib/keys.ts
+++ src/lib/keys.ts
@@ -1,9 +1,3 @@
-export function onNumberEnterKey(fn: (value: number) => void, e: React.KeyboardEvent<HTMLDivElement>) {
-    if (e.key === "Enter") {
-        fn(+e.target.value);
-    }
-}
-
 export function onEnterKey(fn: () => void, e: React.KeyboardEvent<HTMLDivElement>) {
     if (e.key === "Enter") {
         fn();
