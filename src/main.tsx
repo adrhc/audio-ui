459658df4c2917c8fe885ef28ee4a37f816ccdb0
Adrian <adrhc@outlook.com>
2024-04-02 19:12:27 +0300

    createHashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import Volume from './Volume.tsx';
 import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
 
@@ -18,7 +18,7 @@ const theme = createTheme({
   },
 });
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   [
     {
       path: '/',
