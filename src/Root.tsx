b4b4ab014dc05e276b3a79652d606c341525c9cf
Adrian <adrhc@outlook.com>
2024-04-03 10:54:35 +0300

    height: '100%'

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 function Root() {
   return (
-    <Container sx={{ pt: 1, pb: 1, height: '100vh' }}>
+    <Container sx={{ pt: 1, pb: 1, height: '100%' }}>
       <Outlet />
     </Container>
   );
