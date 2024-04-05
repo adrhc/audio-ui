3b950a54545a27e10a9beec76a7dade5299fed73
Adrian <adrhc@outlook.com>
2024-04-05 18:50:41 +0300

    index.html

--- src/ui/VolumeSlider.tsx
+++ src/ui/VolumeSlider.tsx
@@ -1,7 +1,6 @@
 import { VolumeUp } from '@mui/icons-material';
-import { Slider, Stack } from '@mui/material';
+import { Slider, Stack, debounce } from '@mui/material';
 import { useCallback } from 'react';
-import { debounce } from 'lodash';
 import MuteIconButton from './MuteIconButton';
 import { NoParamsProc } from '../lib/types';
 
@@ -16,7 +15,7 @@ export type VolumeSliderParam = {
 };
 
 const VolumeSlider = ({ disabled, mute, volume, setVolume, onMute, onSlide, addLog }: VolumeSliderParam) => {
-  const onSlideFn = useCallback(debounce(onSlide, 150), []);
+  const onSlideFn = useCallback(debounce(onSlide, 100), []);
 
   function handleChange(volume: number) {
     setVolume(volume);
