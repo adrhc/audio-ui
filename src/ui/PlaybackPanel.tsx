228ccdd5874e28e6d9ee342d8cc95d2e23165b15
Adrian <adrhc@outlook.com>
2024-04-09 17:26:57 +0300

    PlaybackPanel

--- src/ui/PlaybackPanel.tsx
+++ src/ui/PlaybackPanel.tsx
@@ -25,7 +25,7 @@ const SX: Record<string, Styles> = {
     fontSize: iconFontSizeMap((ifs) => ifs.map((n, i) => n + (i == 0 ? 1 : 0.75))),
   },
   pl: {
-    fontSize: iconFontSizeMap((ifs) => ifs.map((n, i) => n + (i == 0 ? 0 : 0.25))),
+    fontSize: iconFontSizeMap((ifs) => ifs.map((n, i) => n + (i == 0 ? 0.75 : 0.5))),
   },
 };
 
