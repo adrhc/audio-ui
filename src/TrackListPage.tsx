b53838dc9a977117385c82eadd4de2cbcbdf72e2
Adrian <adrhc@outlook.com>
2024-04-10 22:03:10 +0300

    pb: [1.65

--- src/TrackListPage.tsx
+++ src/TrackListPage.tsx
@@ -80,7 +80,7 @@ const TrackListPage = () => {
     <Stack
       spacing={1}
       sx={{
-        pb: [1.75, 0],
+        pb: [1.65, 0],
         height: '100%',
         justifyContent: 'center',
       }}
