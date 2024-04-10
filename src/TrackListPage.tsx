ee7848c5d2128a2cbd44af84a2af669f9fd8fd44
Adrian <adrhc@outlook.com>
2024-04-10 16:16:14 +0300

    autoFocus

--- src/TrackListPage.tsx
+++ src/TrackListPage.tsx
@@ -86,7 +86,6 @@ const TrackListPage = () => {
       }}
     >
       <List
-        autoFocus={true}
         sx={{
           p: 0,
           overflow: 'auto',
@@ -100,6 +99,7 @@ const TrackListPage = () => {
           .map((sa, i) => (
             <ListItemButton
               key={i}
+              autoFocus={sa.tlid == state.current.tlid}
               selected={sa.tlid == state.current.tlid}
               sx={{ px: 0.5, py: [1, 0.25], border: 'solid thin rgba(0, 0, 0, 0.2)' }}
               onClick={() => handleSelection(sa)}
