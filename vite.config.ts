df908c25347ee2f1d773466f8ff17c14a929d0d0
Adrian <adrhc@outlook.com>
2024-04-19 18:18:36 +0300

    /api/keflsx

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,12 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     proxy: {
-      '/audio-ui': {
+      /* '/audio-ui': {
         target: "https://adrhc.go.ro",
         changeOrigin: true,
-      },
+      }, */
       '/api/keflsx': {
-        target: "http://192.168.1.31:8085/api/keflsx",
+        target: "http://192.168.1.31:8085",
         changeOrigin: true,
       },
       '/mopidy': {
