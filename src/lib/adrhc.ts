5aaea9221e6bf75f32ff2f26a087d8af0fef1de5
Adrian <adrhc@outlook.com>
2024-04-19 21:56:47 +0300

    isAdrhc

--- src/lib/adrhc.ts
+++ src/lib/adrhc.ts
@@ -1,6 +1,5 @@
 export function isAdrhc() {
   return (
-    window.location.hostname == 'localhost' ||
     window.location.hostname == '192.168.1.31' ||
     window.location.hostname == 'adrhc.go.ro'
   );
