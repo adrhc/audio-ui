025049c35a7bd80c803e05e1f92eb6f3e644bb35
Adrian <adrhc@outlook.com>
2024-04-20 18:33:16 +0300

    btnStyle

--- src/ui/panel/MopidyPlayOptions.tsx
+++ src/ui/panel/MopidyPlayOptions.tsx
@@ -58,7 +58,7 @@ const MopidyPlayOptions = () => {
     };
   }, [mopidy]);
 
-  const btnStyle = useBreakpointValue({ p: 1 }, { p: 0.75 });
+  const btnStyle = useBreakpointValue({ p: 1.1 }, { p: 0.75 });
   const iconStyle = { fontSize: iconFontSizeMap((ifs) => ifs.map((n, i) => n + (i == 0 ? 1 : 0.5))) };
 
   return (
