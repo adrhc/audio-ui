c54efe9d04b8c1c146360d4adb531d87035e9b93
Adrian <adrhc@outlook.com>
2024-04-21 12:24:16 +0300

    CornerButton

---
+++ src/components/button/cornerbutton/CornerButton.tsx
@@ -0,0 +1,34 @@
+import { Box } from '@mui/material';
+import { NoArgsProc, Styles } from '../../../lib/types';
+import styles from './styles.module.scss';
+import { useNavigate } from 'react-router-dom';
+
+type CornerButtonParam = {
+  className?: string;
+  sx?: Styles;
+  position?: 'left' | 'right';
+  onClick?: NoArgsProc;
+  to?: string;
+};
+
+const CornerButton = ({ className, sx, position, onClick, to }: CornerButtonParam) => {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    if (onClick) {
+      onClick();
+    } else if (to != null) {
+      navigate(to);
+    }
+  }
+
+  return (
+    <Box
+      className={`${styles.cornerButton} ${styles[position ?? 'left']} ${className}`}
+      sx={sx}
+      onClick={handleClick}
+    ></Box>
+  );
+};
+
+export default CornerButton;
--- src/pages/player/PlayerPage.tsx
+++ src/pages/player/PlayerPage.tsx
@@ -29,6 +29,7 @@ import KefLSXPanel from '../../components/panel/KefLSXPanel';
 import { isAdrhc } from '../../lib/adrhc';
 import '../../components/panel/panel.scss';
 import styles from './styles.module.scss';
+import CornerButton from '../../components/button/cornerbutton/CornerButton';
 
 type VolumePageState = {
   pbStatus?: PlaybackState;
@@ -153,59 +154,63 @@ export default function PlayerPage() {
   }
 
   return (
-    <Stack sx={{ height: '100%', alignItems: 'center' }}>
-      <Stack className={styles.container} spacing={1} sx={{ '& > div': { height: panelHeight } }}>
-        <Box sx={{ height: 'auto !important' }}>
-          <Typography variant="h6" className={styles.title}>
-            {state.songAndArtists.song}
-          </Typography>
-          <Typography variant="h6" className={styles.title}>
-            {state.songAndArtists.artists}
-          </Typography>
-        </Box>
-        <ExactVolumePanel
-          disabled={!online}
-          // exactVolume={exactVolume}
-          // setExactVolume={setExactVolume}
-          onChange={onVolumeChange}
-        />
-        {/* <VolumeSlider
-          disabled={!online}
-          mute={state.mute}
-          volume={state.volume}
-          onMute={() => muteMopidy(mopidy, !state.mute)}
-          onSlide={onVolumeChange}
-        /> */}
-        <PlaybackPanel
-          disabled={!online}
-          status={state.pbStatus}
-          mute={state.mute}
-          onMute={() => muteMopidy(mopidy, !state.mute)}
-          pause={() => pauseMopidy(mopidy)}
-          stop={() => stopMopidy(mopidy)}
-          play={() => safelyPlayCurrent(mopidy)}
-          resume={() => resumeMopidy(mopidy)}
-        />
-        <VolumeButtons
-          disabled={!online}
-          badgeColor="info"
-          volume={state.volume}
-          handleExactVolume={onVolumeChange}
-        />
-        <PrevNextPanel
-          disabled={!online}
-          previous={() => previous(mopidy)}
-          next={() => next(mopidy)}
-          toggleTune={() => setState((old) => ({ ...old, tuneOn: !state.tuneOn }))}
-        />
-        <ShowIf condition={state.tuneOn}>
-          <MopidyPlayOptions />
-        </ShowIf>
-        <ShowIf condition={adrhc && state.tuneOn}>
-          <KefLSXPanel />
-        </ShowIf>
+    <>
+      <Stack sx={{ height: '100%', alignItems: 'center' }}>
+        <Stack
+          className={styles.container}
+          spacing={1}
+          sx={{ '& > div:not(.corner)': { height: panelHeight }, position: 'relative' }}
+        >
+          <Box sx={{ height: 'auto !important' }}>
+            <Typography variant="h6" className={styles.title}>
+              {state.songAndArtists.song}
+            </Typography>
+            <Typography variant="h6" className={styles.title}>
+              {state.songAndArtists.artists}
+            </Typography>
+          </Box>
+          <ExactVolumePanel disabled={!online} onChange={onVolumeChange} />
+          <PlaybackPanel
+            disabled={!online}
+            status={state.pbStatus}
+            mute={state.mute}
+            onMute={() => muteMopidy(mopidy, !state.mute)}
+            pause={() => pauseMopidy(mopidy)}
+            stop={() => stopMopidy(mopidy)}
+            play={() => safelyPlayCurrent(mopidy)}
+            resume={() => resumeMopidy(mopidy)}
+          />
+          <VolumeButtons
+            disabled={!online}
+            badgeColor="info"
+            volume={state.volume}
+            handleExactVolume={onVolumeChange}
+          />
+          <PrevNextPanel
+            disabled={!online}
+            previous={() => previous(mopidy)}
+            next={() => next(mopidy)}
+            toggleTune={() => setState((old) => ({ ...old, tuneOn: !state.tuneOn }))}
+          />
+          <ShowIf condition={state.tuneOn}>
+            <MopidyPlayOptions />
+          </ShowIf>
+          <ShowIf condition={state.tuneOn && adrhc}>
+            <KefLSXPanel />
+          </ShowIf>
+          <CornerButton
+            className="corner"
+            sx={{ position: 'absolute', left: 0, bottom: 0 }}
+            to="/keflsx"
+          />
+          {/* <CornerButton
+            className="corner"
+            sx={{ position: 'absolute', right: 0, bottom: 0 }}
+            position="right"
+          /> */}
+        </Stack>
+        {/* <Logs logs={logs} /> */}
       </Stack>
-      {/* <Logs logs={logs} /> */}
-    </Stack>
+    </>
   );
 }
