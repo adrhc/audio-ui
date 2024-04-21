48c441e22870a55918869cdd14b31aa2d3a48d6d
Adrian <adrhc@outlook.com>
2024-04-21 11:07:21 +0300

    main

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom/client';
-import Dashboard from './pages/Dashboard.tsx';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
