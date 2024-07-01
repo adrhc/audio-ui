8c0ad87953cb9f0a0245e304c5a58e6159f55620
Adrian <adrhc@outlook.com>
2024-07-01 22:54:03 +0300

    useCachedList

--- src/hooks/list/useSongsList.ts
+++ src/hooks/list/useSongsList.ts
@@ -1,7 +1,6 @@
 import Mopidy from 'mopidy';
-import { RefObject, useCallback, useContext } from 'react';
+import { useCallback, useContext } from 'react';
 import { AppContext } from '../../components/app/AppContext';
-import { SustainVoidFn, useSustainableState } from '../useSustainableState';
 import {
   addSongsAndRemember,
   addSongThenPlay,
@@ -9,12 +8,11 @@ import {
   addYtMusicPlAfterAndRemember,
   addYtMusicPlAndRemember,
 } from '../../services/tracklist';
-import { LoadingState, SetLoadingState } from '../../lib/sustain';
-import useScroll from '../useScroll';
+import { LoadingState } from '../../lib/sustain';
 import { Song, isYtMusicPl } from '../../domain/song';
 import { TrackSong } from '../../domain/track-song';
 import { AddAllSongsFn } from '../../components/list/navigator-commons';
-import { ScrollToFn } from '../../domain/scroll';
+import useCachedList, { UseCachedList } from './useCachedList';
 
 export type RawSongsPageState = {
   songs: Song[];
@@ -30,54 +28,32 @@ export function pickRawSongsPageState(rawState?: RawSongsPageState | null) {
       } as RawSongsPageState);
 }
 
-export interface UseSongsList<P extends RawSongsPageState> {
-  state: LoadingState<P>;
-  sustain: SustainVoidFn<P>;
-  setState: SetLoadingState<P>;
+export interface UseSongsList<S extends RawSongsPageState> extends UseCachedList<S> {
   handleSelection: (song: Song) => void;
   handleAdd: (song: Song) => void;
   handleAddAll: AddAllSongsFn;
   handleInsert: (song: Song) => void;
-  listRef: RefObject<HTMLUListElement>;
-  scrollObserver: (e: React.UIEvent<HTMLUListElement>) => void;
-  scrollTo: ScrollToFn;
   currentSong?: TrackSong;
   mopidy?: Mopidy;
 }
 
-export default function useSongsList<P extends RawSongsPageState>(
+export default function useSongsList<S extends RawSongsPageState>(
   cacheName: string,
-  defaultState?: Partial<LoadingState<P>> | null
-): UseSongsList<P> {
-  const { getCache, mergeCache } = useContext(AppContext);
-  const cache = getCache(cacheName) as P;
-  const [scrollTo, listRef] = useScroll<HTMLUListElement>();
-  const [state, sustain, setState] = useSustainableState<P>({
+  defaultState?: Partial<LoadingState<S>> | null
+): UseSongsList<S> {
+  const { sustain, ...cachedListRest } = useCachedList<S>(cacheName, {
     songs: [],
     ...defaultState,
-    ...(cache as object),
-  } as LoadingState<P>);
+  } as LoadingState<S>);
   const { mopidy, currentSong } = useContext(AppContext);
   // console.log(`[useSongsList]`, { cache, state });
-
-  const scrollObserver = useCallback(
-    (e: React.UIEvent<HTMLUListElement>) => {
-      const scrollTop = e.currentTarget.scrollTop;
-      // console.log(`[useSongsList:scrollObserver] new scrollTop:`, scrollTop);
-      mergeCache(cacheName, (old) => {
-        // console.log(`[scrollObserver] old scrollTop = ${getScrollTop(old)}, new scrollTop = ${scrollTop}`);
-        return { ...(old as object), scrollTop };
-      });
-    },
-    [cacheName, mergeCache]
-  );
 
   const handleSelection = useCallback(
     (song: Song) => {
       // console.log(`[useSongsList:handleSelection] song:\n`, song);
       sustain(
-        addSongThenPlay(mopidy, song)?.then(() => ({ lastUsed: song }) as Partial<P>),
-        { error: `Failed to start ${song.title}!`, lastUsed: song } as Partial<LoadingState<P>>
+        addSongThenPlay(mopidy, song)?.then(() => ({ lastUsed: song }) as Partial<S>),
+        { error: `Failed to start ${song.title}!`, lastUsed: song } as Partial<LoadingState<S>>
       );
     },
     [mopidy, sustain]
@@ -88,7 +64,7 @@ export default function useSongsList<P extends RawSongsPageState>(
       // console.log(`[useSongsList:handleAddAll] songs:\n`, songs);
       sustain(addSongsAndRemember(mopidy, ...songs), {
         error: `Failed to add ${songs.length} songs!`,
-      } as Partial<LoadingState<P>>);
+      } as Partial<LoadingState<S>>);
     },
     [mopidy, sustain]
   );
@@ -99,8 +75,8 @@ export default function useSongsList<P extends RawSongsPageState>(
       // addYTMPlAndRemember uses /playlist/content instead of Mopidy to replace "ytmusic:" with "youtube:"
       const addFn = isYtMusicPl(song) ? addYtMusicPlAndRemember : addSongsAndRemember;
       sustain(
-        addFn(mopidy, song)?.then(() => ({ lastUsed: song }) as Partial<P>),
-        { error: `Failed to add ${song.title}!`, lastUsed: song } as Partial<LoadingState<P>>
+        addFn(mopidy, song)?.then(() => ({ lastUsed: song }) as Partial<S>),
+        { error: `Failed to add ${song.title}!`, lastUsed: song } as Partial<LoadingState<S>>
       );
     },
     [mopidy, sustain]
@@ -112,24 +88,20 @@ export default function useSongsList<P extends RawSongsPageState>(
       // console.log(`[useSongsList:onAdd] song:\n`, song);
       const addAfterFn = isYtMusicPl(song) ? addYtMusicPlAfterAndRemember : addSongsAfterAndRemember;
       sustain(
-        addAfterFn(mopidy, currentSong, song)?.then(() => ({ lastUsed: song }) as Partial<P>),
-        { error: `Failed to start ${song.title}!`, lastUsed: song } as Partial<LoadingState<P>>
+        addAfterFn(mopidy, currentSong, song)?.then(() => ({ lastUsed: song }) as Partial<S>),
+        { error: `Failed to start ${song.title}!`, lastUsed: song } as Partial<LoadingState<S>>
       );
     },
     [mopidy, sustain, currentSong]
   );
 
   return {
-    state,
+    ...cachedListRest,
     sustain,
-    setState,
     handleSelection,
     handleAdd,
     handleAddAll,
     handleInsert,
-    listRef,
-    scrollTo,
-    scrollObserver,
     currentSong,
     mopidy,
   };
