1d3d5e956be664246d5392d75038a973e02385f0
adrhc <adrhc@outlook.com>
2024-09-21 19:49:48 +0300

    192.168.0.1

--- src/lib/adrhc.ts
+++ src/lib/adrhc.ts
@@ -7,9 +7,9 @@ export function isLocalAdrhc(includePi?: boolean) {
   return (
     window.location.hostname == 'localhost' ||
     window.location.hostname == '127.0.0.1' ||
+    window.location.hostname == '192.168.0.1' ||
     window.location.hostname == '192.168.1.31' ||
-    (includePi && window.location.hostname == '192.168.1.32') ||
-    window.location.hostname == '192.168.0.31' ||
-    (includePi && window.location.hostname == '192.168.0.32')
+    (includePi && window.location.hostname == '192.168.0.32') ||
+    (includePi && window.location.hostname == '192.168.1.32')
   );
 }
