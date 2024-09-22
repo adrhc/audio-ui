d41dbb7ba915a518285590e98f048235d4d3a37c
adrhc <adrhc@outlook.com>
2024-09-22 21:28:31 +0300

    doubleEncodedUri

--- src/services/audio-ws/audio-ws.ts
+++ src/services/audio-ws/audio-ws.ts
@@ -43,7 +43,18 @@ export function createPlaylist(name: string) {
  * alternative to getPlItems from mpc.ts
  */
 export function getPlContent(imgMaxArea: number, uri: string): Promise<Song[]> {
-  return get<audiows.RefWithImages[]>(`${MOPIDY_PLAYLIST}/${encodeURIComponent(uri)}`).then((rwis) =>
+  // Nginx decodes 1x the received requests so 1x encoded "m3u:[Radio Streams].m3u8" 
+  // becomes "m3u:[Radio Streams].m3u8" when passed to upstream (aka, java).
+  // 
+  // "m3u:[Radio Streams].m3u8" containes special characters (i.e., space [ and ]) 
+  // which must be encoded, otherwise the request is not passed to the upstream!
+  // 
+  // 2x encoded "m3u:[Radio Streams].m3u8" becomes "m3u%3A%5BRadio%20Streams%5D.m3u8"
+  // for the upstream; PlaylistsService.getPlItems pass it to Mopidy but Mopidy can't
+  // decode the encoded : character (i.e. %3A); solved with java.net.URLDecoder.decode.
+  const doubleEncodedUri = encodeURIComponent(encodeURIComponent(uri));
+  console.log(`[getYTPlContent] double encoded ytUri = `, doubleEncodedUri);
+  return get<audiows.RefWithImages[]>(`${MOPIDY_PLAYLIST}/${doubleEncodedUri}`).then((rwis) =>
     audiows.refWithImagesToSongs(imgMaxArea, rwis)
   );
 }
