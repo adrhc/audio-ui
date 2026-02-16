aedc3c24e5d717b6dda59b8933f7d9d0652e4c36
adrhc <adrhc@outlook.com>
2026-02-16 21:03:27 +0200

    + LocalPlContentPage.onDelete

--- src/infrastructure/audio-db/plentry.ts
+++ src/infrastructure/audio-db/plentry.ts
@@ -2,6 +2,6 @@ import { removeVoid } from '../../lib/rest';
 
 export const PLAYLIST_ENTRY = '/audio-ui/db-api/plentry';
 
-export function removeFromLocalPl(plUri: string, songUri: string, songTitle: string): Promise<void> {
-  return removeVoid(PLAYLIST_ENTRY, JSON.stringify({ plUri, songUri, songTitle }));
+export function removeFromLocalPl(plFileName: string, songUri: string, songTitle: string): Promise<void> {
+  return removeVoid(PLAYLIST_ENTRY, JSON.stringify({ plFileName, songUri, songTitle }));
 }
